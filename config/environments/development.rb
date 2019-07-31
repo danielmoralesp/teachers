936fa872b2750652bf07d7a90cc075fcea9e7d00
danielmoralesp <daniel_moralesp@hotmail.com>
2019-07-30 20:29:10 -0500

    Ticket #1 - Devise mailer - reset password instructions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,8 @@ Rails.application.configure do
 
   # Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Mailcathcer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
