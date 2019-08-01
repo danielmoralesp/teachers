4c1fa8e4a4ee308d3282e0ecd7b62fa167f18642
danielmoralesp <daniel_moralesp@hotmail.com>
2019-08-01 18:52:15 -0500

    Ticket #2 - Add City to users and create/edit user with city

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+	before_action :add_parameters_to_user, if: :devise_controller?
+
+	protected
+
+		def add_parameters_to_user
+			devise_parameter_sanitizer.permit(:sign_up, keys: [:city_id])
+			devise_parameter_sanitizer.permit(:account_update, keys: [:city_id])
+		end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  belongs_to :city
+
   after_create :send_welcome_email
 
   def send_welcome_email
