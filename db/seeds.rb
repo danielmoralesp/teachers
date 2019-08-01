47cc72f9a80803ac5dbb51326f830ca39ae555f5
danielmoralesp <daniel_moralesp@hotmail.com>
2019-07-31 21:02:50 -0500

    Ticket #2 - Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+City.delete_all
+City.create([
+	{ name: 'Medellin' }, 
+	{ name: 'Bogota' },
+	{ name: "Cali" },
+	{ name: "Barranquilla" },
+	{ name: "Manizales" },
+	{ name: "Cartagena" },
+	{ name: "Bucaramanga" }
+])
