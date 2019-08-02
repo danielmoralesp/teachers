d6819d71887bcd461c0f2c1978ef3ed8548d65df
danielmoralesp <daniel_moralesp@hotmail.com>
2019-08-02 18:49:48 -0500

    Ticket #2 -Avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
 	before_action :add_parameters_to_user, if: :devise_controller?
+	before_action :avatar
+	before_action :large_avatar
 
 	protected
 
 		def add_parameters_to_user
 			devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :city_id])
 			devise_parameter_sanitizer.permit(:account_update, keys: [:role, :city_id])
+		end
+
+		def avatar
+			@avatar = user_signed_in? ? current_user.avatar_url : nil
+		end
+
+		def large_avatar
+			avatar_url = user_signed_in? ? current_user.avatar_url : nil
+			@large_avatar = "#{avatar_url}&s=100"
 		end
 end
